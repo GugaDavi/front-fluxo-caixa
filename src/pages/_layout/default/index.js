df7ae66d780874b0d4de897f402dbc5627feab01
GugaDavi <gguedes.tech@gmail.com>
2019-09-23 08:40:44 -0300

    Default Layout

--- src/pages/_layout/default/index.js
+++ src/pages/_layout/default/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import Header from '../../../components/Header';
 
-export default function Default() {
-  return <div />;
+import { Wrapper, Content } from './styles';
+
+export default function Default({ children }) {
+  return (
+    <Wrapper>
+      <Header />
+      <Content>{children}</Content>
+    </Wrapper>
+  );
 }
+
+Default.propTypes = {
+  children: PropTypes.element.isRequired,
+};
